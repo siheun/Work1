b7a0ebbfa236ccefc2e5f040747350a500b75dc5
Siheun Kim <pineblossom88@gmail.com>
2018-01-29 14:12:56 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -38,7 +38,7 @@ function draw() {
   pointLight(255, 255, 255, 0, -200, 0);
   //pointLight(255, 0, 255, 200, 0, 0);
 
-  background(175);
+  background(255);
 
  for (let z = -300 ; z < (height - 300); z += 60){
   for (let x = 150; x < (width - 300) ; x += 60) {
